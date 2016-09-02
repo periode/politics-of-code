6207eb4611f24db897966243d1fc932c5565a746
Pierre Depaz <pierre.depaz@gmail.com>
2016-09-02 17:27:28 +0400

    added examples for the jade templating engine

--- 01_javascript-server/c_serving_and_routing_with_express/server.js
+++ 01_javascript-server/c_serving_and_routing_with_express/server.js
@@ -8,11 +8,21 @@ var http = require('http');
 //path is a module which allows us to make sure the paths to our folders and files are valid
 var path = require('path');
 
+//here we require fs, a basic (as in 'essential') module to read and write files/directories from the server
+var fs = require('fs');
+
 //here we instantiate our server
 var app = express();
 
+var time = new Date(); //we instantiate a new Date() object, cause it's always useful to have the time :)
+
 //here we set the port on which our app is going to listen
 app.set('port', 8080);
+
+//here we set our templating engine
+app.set('view engine', 'jade');
+//setting where the templates live
+app.set('views', 'public/views');
 
 //this is the equivalent of "serveFiles" in servi
 app.use(express.static(path.join(__dirname, 'public')));
@@ -23,7 +33,33 @@ http.createServer(app)//here we tell the http module to start our server, using 
     console.log('the server is now up and running on port',app.get('port')); //which is only going to print a message to our console
   });
 
+app.get('/responses/:title', function(req, res, err){
 
+  //now we're about to render a template by filling it with a bunch of dynamic content
+  //this dynamic content is going to be set through a JSON object, which we're going to give to the response, along with the name of the template we want to render
+
+  //so we start by creating our empty object
+  var content = {
+    title:'',
+    text:'',
+    date:''
+  };
+
+  //the title is going to be fetched directly from the url
+  //so that, if someone requests "http://mywebsite.com/reading/week1", the title will be "week1"
+  content.title = req.params.title;
+
+  //here we use the fs module to read contents of a file, which is going to have the same name as our title ("./public/responses/week1.txt")
+  content.text = fs.readFileSync('./public/responses/'+req.params.title+'.txt', 'utf8');
+
+  //and finally we use our date object to get the current date, so we don't have to hard-coded in our html and change our it everyday :p
+  //the .toString() method in JavaScript just makes sure that whatever we're referencing is, well, a string.
+  content.date = "today's date is: "+time.getUTCDate().toString()+"/"+(time.getMonth()+1).toString()+"/"+time.getFullYear().toString();
+
+  //now we have all of our content, and we send it to the client, along with the template.
+  //upon arrival, they will merge into one beautiful webpage
+  res.render('response.jade', content);
+});
 
 //this is where we set-up our routes
 app.get('/blog', serveBlog);
