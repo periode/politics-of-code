ce3ceba3ee536220298ea80b158daaae0fa4c264
Pierre Depaz <pierre.depaz@gmail.com>
2016-09-26 17:21:19 +0400

    twitter query bot

---
+++ 05_javascript-botmaking/bot_search.js
@@ -0,0 +1,14 @@
+var Twit = require('twit');
+
+var t = new Twit({
+  consumer_key : 'kij47kAvvt6tPIVit99ATR3kq',
+  consumer_secret: 'yirA83ukpyt0QstxtJhIFnLzVU5xbOFWVXe7Q2oTlyGtdwLBpW',
+  access_token: '408032569-GGR6Hba7RAbeFJx5CE7jKAYZ4BiXNhVmQmNGQGGj',
+  access_token_secret: 'GpuKie9Asor6uAf9XImhFqjUDvCcRH2GxOkcfPNbuOyzX',
+  timeout_ms: 60*1000 //we ensure a timeout of one minute after a HTTP request has failed
+});
+
+
+t.get('search/tweets', {q: 'france since:2015-09-28', count: 100}, function(err, data, response){
+  console.log(data);
+});
