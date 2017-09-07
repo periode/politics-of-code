be6beddc1b42e8b0fcb1481c98466d105961ba15
Pierre Depaz <pierre.depaz@gmail.com>
2017-09-07 15:46:14 +0400

    "index html"

--- 01_javascript-server/in_class/server.js
+++ 01_javascript-server/in_class/server.js
@@ -5,9 +5,10 @@ app.listen(2046, function(){
   console.log('we have started the server');
 });
 
+app.use(express.static('public'));
 
 app.get('/', function(request, response, error){
-  response.send('hello there');
+  response.send('<h1>hello there from pierre</h1>');
 });
 
 
