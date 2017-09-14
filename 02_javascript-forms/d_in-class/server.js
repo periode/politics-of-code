225ec8b1de810aef1f028d79482e7e786cc626af
Pierre Depaz <pierre.depaz@gmail.com>
2017-09-14 16:01:11 +0400

    "save users to file"

--- 02_javascript-forms/d_in-class/server.js
+++ 02_javascript-forms/d_in-class/server.js
@@ -1,7 +1,8 @@
-var express = require('express');
+var express = require('express'); 
 var app = express();
 var port = 8000;
 var bp = require('body-parser');
+var fs = require('fs');
 
 app.use(express.static('public'));
 
@@ -17,7 +18,7 @@ app.listen(port, function(){
 //this is called a route
 //this specific route is for the endpoint submit
 app.get("/submit", function(request, response, error){
-  console.log(request.query);
+  console.log(request);
   
   if(request.query.user_religion == 'none'){
     response.send('you cannot be president of the united states');
@@ -27,8 +28,36 @@ app.get("/submit", function(request, response, error){
 });
 
 app.post("/submit", function (request, response, error){
-  console.log(request.body);
-  response.send('hehehehe');
+  console.log('input', request.body);
+  
+  var user = {};
+  user.name = request.body.user_name;
+  user.happiness = 'undefined';
+  console.log('----------');
+  console.log('output', user);
+
+  
+  fs.readFile('./data/users.json', function(error, data){
+    var file = JSON.parse(data);
+    
+    console.log(data);
+    file.all_users.push(user);
+    
+    fs.writeFile('./data/users.json', JSON.stringify(all_users), function(error){
+      
+      if(error){
+        console.log(error);
+      }else{
+        console.log('success! written new user',user);  
+      }
+      
+    });
+  });
+
+  
+  
+  
+  response.send('thank you');
 });
 
 
