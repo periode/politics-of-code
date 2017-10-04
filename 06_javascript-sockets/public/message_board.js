f128c1a19a765a50138639df800058e94885cd70
Pierre Depaz <pierre.depaz@gmail.com>
2017-10-04 16:12:20 +0400

    added socket servers

--- 06_javascript-sockets/public/message_board.js
+++ 06_javascript-sockets/public/message_board.js
@@ -1,11 +1,25 @@
-var socket = io.connect("http://localhost:2046");
+var socket = io.connect("http://10.225.32.152");
 
 socket.on('open', function(){
   console.log('connected');
 });
 
+socket.on('update', function(data){
+  document.getElementById("message-board").innerHTML += data.greet;
+  
+  console.log(data);
+  
+  for(var i = 0; i < data.messages.length; i++){
+    document.getElementById("message-board").innerHTML += data.messages[i];
+  }
+});
+
 socket.on('send-back-message', function(data){
-  console.log('received', data);
+  document.getElementById("message-board").innerText += data;
+});
+
+socket.on('image', function(data){
+  document.getElementById('imagefile').src = data;
 });
 
 function sendRandomStuff(msg){
@@ -15,3 +29,43 @@ function sendRandomStuff(msg){
   socket.emit('message', msg);
 }
 
+
+function sendMessage(){
+  var msg = document.getElementById("input-field").value;
+  
+  socket.emit('message', msg);
+}
+
+var initWebRTC = function() {
+
+  // These help with cross-browser functionality
+  window.URL = window.URL || window.webkitURL || window.mozURL || window.msURL;
+  navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+  
+  // The video element on the page to display the webcam
+  var video = document.getElementById('thevideo');
+
+  // if we have the method
+  if (navigator.getUserMedia) {
+    navigator.getUserMedia({video: true}, function(stream) {
+        video.src = window.URL.createObjectURL(stream) || stream;
+        video.play();
+        // video.style.visibility = "hidden";
+      }, function(error) {alert("Failure " + error.code);});
+  }
+        
+  var thecanvas = document.getElementById('thecanvas');
+  var thecontext = thecanvas.getContext('2d');
+    
+    var draw = function() {
+      console.log('drawing');
+      thecontext.drawImage(video,0,0,640, 480);
+      var dataUrl = thecanvas.toDataURL('image/webp', 1);
+      document.getElementById('imagefile').src = dataUrl;
+      socket.emit('image', dataUrl);
+      setTimeout(draw,3000);
+    };
+
+    if(true)
+      draw();			
+};
--- 06_javascript-sockets/socket-server.js
+++ 06_javascript-sockets/socket-server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path =  require('path');
 var app = express();
 
-var port = 2046;
+var port = 80;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -13,12 +13,43 @@ var server = app.listen(port, function(){
 
 var io = require('socket.io')(server);
 
+var all_messages = [];
+
 io.on('connect', function(socket){
   console.log('new socket connected', socket.id);
   
+  var greeting_package = {
+    greet: "hey there!",
+    messages : all_messages
+  }
+  socket.emit('update', greeting_package);
+  
+  
   socket.on('message', function(data){
-    console.log('receiving',data);
+    console.log('receiving',data, 'from', socket.id);
     
+    all_messages.push(data);
+    
+    //forward messages as they come
     io.emit('send-back-message', data);
   });
+  
+  socket.on('image', function(data){
+    console.log(data);
+    socket.broadcast.emit('image',data);
+  });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
