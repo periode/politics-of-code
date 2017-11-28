a21cf109cf00f84fdc5a761b3ac205ee7aac6875
Han Su <hs2657@nyu.edu>
2017-11-28 15:53:41 +0400

    Add files via upload

--- 12_misc-semantic-code/war/war.js
+++ 12_misc-semantic-code/war/war.js
@@ -1,28 +1,33 @@
-function War(enemies_reason, weapon){
+/**
+ * Created by Suhan on 28/11/2017.
+ */
 
-	this.enemies = Object.keys(enemies_reason)
-
-	this.prevent = function(){
-		this.enemies.forEach(function(enemy) {
-			console.log("Dear " + enemy +
-				", you don't need " + enemies_reason[enemy] + ". Make peace instead.")
-			if (weapon == "nuclear") {
-				var firstEnemy = this.enemies[Math.floor(Math.random()*this.enemies.length)]
-				console.log("\n"+firstEnemy+" will first remove all of their nuclear weapons. Hopefully everyone will follow.")
-			}
-
-		})
-	}
+function War(country_a,country_b) {
+    var force_a = country_a.allies.power? country_a.power + country_a.allies.power : country_a.power;
+    var force_b = country_b.allies.power? country_b.power + country_b.allies.power : country_b.power;
+    if(force_a>force_b){
+        this.winner = country_a.name
+    }else {
+        this.winner = country_b.name
+    }
+    this.length = 100*(force_b-force_a)*(force_b-force_a)
 }
 
-var ww3 = new War({
-					"US":"world power",
-					"Russia":"world power",
-					"China": "world power",
-					"Iran": "more friends",
-					"IS": "a new state"
-				},
-				"nuclear"
-				);
+var DPRK = new Object();
+DPRK.name = "DPRK";
+DPRK.allies = "";
+DPRK.power = 0.5;
 
-ww3.prevent()
+
+var ROK = new Object();
+ROK.name = "ROK";
+ROK.allies = US;
+ROK.power = 0.9;
+
+var US = new Object();
+US.name = "US";
+US.allies = ROK;
+US.power = 0.9;
+
+var war = new War(DPRK,US);
+console.log(war.winner,"wins war after",war.length,"days")
